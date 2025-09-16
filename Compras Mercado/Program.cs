8779bf78f0e6ad81090d6c4cf1e9324bf264a346
Cub0-ux <83610305+Cub0-ux@users.noreply.github.com>
2025-09-16 20:55:13 -0300

    inicio

--- Compras Mercado/Program.cs
+++ Compras Mercado/Program.cs
@@ -5,16 +5,33 @@
         static void Main(string[] args)
         {
             //Adicionar produto ao carrinho
-            Console.WriteLine("Digite o nome do produto:");
-            string name = Console.ReadLine();
-            Console.WriteLine("Digite o preço do produto:");
-            double price = double.Parse(Console.ReadLine());
+            string[] produtoNome = new string[10];
+            double[] valorProd = new double[10];
+            int indice = 0;
+            Console.Write("Adicione máximo de 10 produtos!\n");
+            Console.Clear();    
+            bool rodando = true;
+            while(rodando)
+            {
+                CadNome();
+                Console.WriteLine("Digite o preço do produto:");
+                double price = double.Parse(Console.ReadLine());
+            }
 
             //Listar produtos no carrinho
 
             //Ver valor total da compra
 
             //Finalizar compra e sair do sistema
+
+
+            //Métodos:
+            static string CadNome()
+            {
+                Console.WriteLine("Digite o nome do produto:");
+                string nome = Console.ReadLine();
+                return nome;
+            }
 
         }
     }
