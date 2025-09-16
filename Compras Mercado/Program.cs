95e1f033e92ad6a2b14fc1b8018cf82e856b2352
Cub0-ux <83610305+Cub0-ux@users.noreply.github.com>
2025-09-16 20:20:00 -0300

    Adicionar arquivos de projeto.

---
+++ Compras Mercado/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Compras_Mercado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
